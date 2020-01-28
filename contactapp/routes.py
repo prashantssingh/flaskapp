4e99539bf5821d88755e024956926b1b71e479bf
Prashant Singh <prashant_singh@live.in>
2020-01-27 23:48:47 -0600

    contactapp: add form handling

--- contactapp/routes.py
+++ contactapp/routes.py
@@ -1,5 +1,5 @@
 from contactapp import app
-from flask import render_template
+from flask import render_template, flash, redirect
 from app.forms import LoginForm
 
 @app.route('/')
@@ -22,4 +22,9 @@ def index():
 @app.route('/login')
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+ 
     return render_template('login.html', title='Sign In', form=form)
