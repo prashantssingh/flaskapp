f76c5fa7283dee464b900f3a8b58c7477d312d9e
Prashant Singh <prashant_singh@live.in>
2020-01-31 23:18:51 -0600

    contactapp: add exports in init file

--- contactapp/__init__.py
+++ contactapp/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-from contactapp import routes
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from contactapp import routes, models
